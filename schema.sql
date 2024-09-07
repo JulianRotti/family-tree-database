4591bfc1cc9dac8f08a7f0ea8e339c5541a21b0c
JulianRotti <julianrottenheimer1989@gmail.com>
2024-09-07 22:18:16 +0200

    add family tree logic

--- schema.sql
+++ schema.sql
@@ -6,6 +6,7 @@ SET FOREIGN_KEY_CHECKS = 0;
 
 DROP TABLE IF EXISTS members;
 DROP TABLE IF EXISTS relationships;
+DROP PROCEDURE IF EXISTS get_family_tree;
 
 SET FOREIGN_KEY_CHECKS = 1;
 
@@ -43,3 +44,73 @@ BEGIN
 END //
 
 DELIMITER ;
+
+DELIMITER //
+
+CREATE PROCEDURE get_family_tree(IN memberId INT)
+BEGIN
+    DROP TABLE IF EXISTS temp_combined;
+    -- Create a temporary table to hold combined results
+    CREATE TABLE temp_combined AS
+
+    -- Use a CTE to get descendants
+    WITH RECURSIVE descendants AS (
+        -- Base case: Start with the given member
+        SELECT 
+            r.member_1_id AS member_1_id,
+            m.id AS member_2_id,
+            'parent' AS relationship
+        FROM 
+            members m
+        JOIN 
+            relationships r ON m.id = r.member_2_id
+        WHERE 
+            r.relationship = 'parent' 
+            AND r.member_1_id = memberId
+        
+        UNION ALL
+        
+        -- Recursive case: Find descendants of the previously found descendants
+        SELECT 
+            r.member_1_id AS member_1_id,
+            m.id AS member_2_id,
+            'parent' AS relationship
+        FROM 
+            members m
+        JOIN 
+            relationships r ON m.id = r.member_2_id
+        JOIN 
+            descendants d ON r.member_1_id = d.member_2_id
+        WHERE 
+            r.relationship = 'parent'
+    ),
+    spouses AS (
+        -- Find spouses of the members in the descendants CTE
+        SELECT 
+            r.member_1_id AS member_1_id,
+            r.member_2_id AS member_2_id,
+            'spouse' AS relationship
+        FROM 
+            relationships r
+        WHERE 
+            (r.member_1_id IN (SELECT member_1_id FROM descendants) 
+            OR r.member_2_id IN (SELECT member_2_id FROM descendants))
+            AND r.relationship = 'spouse'
+    )
+    -- Insert descendants and spouses into temp_combined
+    SELECT * FROM descendants
+    UNION ALL
+    SELECT * FROM spouses;
+
+    -- First result set: Return the combined relationships
+    SELECT * FROM temp_combined;
+
+    SELECT * FROM members
+    WHERE id IN (SELECT member_2_id FROM temp_combined UNION SELECT member_1_id FROM temp_combined);
+
+    -- Cleanup: Drop the temporary table
+    DROP TABLE IF EXISTS temp_combined;
+END //
+
+DELIMITER ;
+
